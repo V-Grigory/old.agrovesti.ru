4750eace876c8d6c09d054ae88994c7710aa3677
Grigory Volchok <g.v.volchok@tmn2.etagi.com>
2018-07-25 13:29:14 +0500

    add yandex metrika and counter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta name="yandex-verification" content="bcc728036f546b84" />
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -51,6 +53,37 @@
                 closedir($handle);
             }
         @endphp
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+          (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+              try {
+                w.yaCounter47604658 = new Ya.Metrika({
+                  id:47604658,
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+                });
+              } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+              s = d.createElement("script"),
+              f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+              d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+          })(document, window, "yandex_metrika_callbacks");
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/47604658" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+
     </head>
 
     <body>
@@ -217,6 +250,15 @@
 
                 </div>
             </div>
+
+            <!-- Yandex.Metrika informer -->
+            <a href="https://metrika.yandex.ru/stat/?id=47604658&amp;from=informer"
+               target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/47604658/3_1_FFFFFFFF_EFEFEFFF_0_uniques"
+                                                   style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика"
+                                                   title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)"
+                                                   class="ym-advanced-informer" data-cid="47604658" data-lang="ru" />
+            </a>
+            <!-- /Yandex.Metrika informer -->
 
             <hr style="margin-bottom: 0px;border: none;">
             <div class="footer_copyright">
